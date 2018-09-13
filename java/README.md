8e15fc0542460da6d7b888f819e0ca2f4684b89b
warriorg <warrior.gao@gmail.com>
2018-09-13 21:28:56 +0800

    add 23 disign patterns

--- java/README.md
+++ java/README.md
@@ -1,4 +1,5 @@
-
-* mybatis mybatis 实践
-* spring spring 实践
+* basic java基础/设计模式
+* mybatis mybatis实践
+* spring spring实践
+* netty netty实践
 
---
+++ java/basic/src/me/warriorg/design/creational/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package me.warriorg.design.creational.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
