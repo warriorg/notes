66d1c9a1f380c4a0906c83fe3f5aa4b8c506f40d
warriorg <warrior.gao@gmail.com>
2020-07-22 11:09:31 +0800

    update study

--- algorithms/algorithms/src/datastructures/stack/stack.go
+++ algorithms/algorithms/src/datastructures/stack/stack.go
@@ -1,31 +1,83 @@
 package main
 
-type MyQueue struct {
+import (
+	"fmt"
+	"strings"
+)
+
+type ArrayStack struct {
+	data []int // 存放数据的位置
+	top  int   // 栈顶指针
 }
 
 /** Initialize your data structure here. */
-func Constructor() MyQueue {
+func Constructor() *ArrayStack {
+	return &ArrayStack{
+		data: make([]int, 0, 32),
+		top:  -1,
+	}
+}
 
+/** Size  */
+func (this *ArrayStack) Size() int {
+	return this.top + 1
 }
 
 /** Push element x to the back of queue. */
-func (this *MyQueue) Push(x int) {
-
+func (this *ArrayStack) Push(x int) {
+	this.top += 1
+	if this.top > len(this.data)-1 {
+		this.data = append(this.data, x)
+	} else {
+		this.data[this.top] = x
+	}
 }
 
 /** Removes the element from in front of queue and returns that element. */
-func (this *MyQueue) Pop() int {
-
+func (this *ArrayStack) Pop() int {
+	if this.Empty() {
+		return -1
+	}
+	x := this.data[this.top]
+	this.top -= 1
+	return x
 }
 
 /** Get the front element. */
-func (this *MyQueue) Peek() int {
-
+func (this *ArrayStack) Peek() int {
+	if this.Empty() {
+		return -1
+	}
+	return this.data[this.top]
 }
 
 /** Returns whether the queue is empty. */
-func (this *MyQueue) Empty() bool {
+func (this *ArrayStack) Empty() bool {
+	return this.top == -1
+}
 
+/** String pring string */
+func (this *ArrayStack) String() string {
+	if this.Empty() {
+		return "empty stack"
+	}
+	return strings.Trim(strings.Replace(fmt.Sprint(this.data), " ", ",", -1), "[]")
+}
+
+func main() {
+	obj := Constructor()
+	obj.Push(2)
+	obj.Push(6)
+	obj.Push(8)
+	obj.Push(9)
+
+	fmt.Println(obj)
+	val := obj.Pop()
+	fmt.Println(val)
+	val = obj.Peek()
+	fmt.Println(val)
+	b := obj.Empty()
+	fmt.Println(b)
 }
 
 /**
